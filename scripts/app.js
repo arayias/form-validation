12d19a94b232914db2d110165655d15a851e6b2c
Andreas Rayias <wowcaeu.andreas@gmail.com>
2023-09-15 17:24:43 +0100

    Added some of the logic that will be used to invalidate the form inputs and display error messages to the user. Add styling and error messages to the form inputs.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,31 @@
-console.log("test");
+const form = document.querySelector("form");
+
+const nameInput = document.querySelector("#name");
+const surnameInput = document.querySelector("#surname");
+const passwordInput = document.querySelector("#password");
+const emailInput = document.querySelector("#email");
+const phoneInput = document.querySelector("#phone");
+
+const regExp = {
+  name: {
+    regExp: /^[a-zA-Z]{2,}$/,
+    message: "Name should contain at least 2 alphabetic characters",
+  },
+  surname: {
+    regExp: /^[a-zA-Z]{2,}$/,
+    message: "Surname should contain at least 2 alphabetic characters",
+  },
+  password: {
+    regExp: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+    message:
+      "Password should contain at least 8 characters, 1 uppercase letter, 1 lowercase letter and 1 number",
+  },
+  email: {
+    regExp: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+    message: "Invalid email",
+  },
+  phone: {
+    regExp: /^\+?3?8?(0\d{9})$/,
+    message: "Invalid phone number",
+  },
+};
